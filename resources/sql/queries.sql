540b82d2c4012d3f78cf791b1410c08359d07a55
Dmitry Matveyev <dm.matveyev@protonmail.com>
2018-07-19 19:53:51 +0300

    Add queries to insert and delete from database

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,5 +1,22 @@
 -- :name get-thesaurus :? :*
--- :doc loads all thesaurus into memory
+-- :doc Loads all thesaurus into memory.
 SELECT w.word, an.antonym
 FROM words w
 JOIN antonyms an ON w.id = an.word_id
+
+-- :name insert-word! :i! :raw
+-- :doc Inserts new word to find for replacement.
+INSERT INTO words (word) VALUES (:word)
+
+-- :name insert-antonym! :i! :raw
+-- :doc Inserts antonyms to corresponding words.
+INSERT INTO antonyms (word_id, antonym) VALUES (:word_id, :antonym)
+
+-- :name delete-word! :! :n
+-- :doc Deletes a word by name. WARNING: also deletes all referenced antonyms.
+DELETE FROM words WHERE word = :word
+
+-- :name delete-antonym! :! :n
+-- :doc Deletes an antonym by name from the set referencing given word.
+DELETE FROM antonyms WHERE antonym = :antonym
+AND word_id = (SELECT id FROM words WHERE word = :word)
